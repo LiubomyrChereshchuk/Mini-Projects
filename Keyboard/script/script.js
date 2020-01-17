f3e89dbebc8caf93d5d7ac3be5478f768ecb4c81
Liubomyr <43443223+Lub4ikChe@users.noreply.github.com>
2020-01-17 15:42:14 +0200

    Update script.js

--- Keyboard/script/script.js
+++ Keyboard/script/script.js
@@ -1,6 +1,8 @@
-const numAndLetter = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 90, 88, 67, 86, 66, 78, 77];
+const numAndLetter = [49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 81, 87, 69, 82, 84, 89,
+                      85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 90, 88, 67, 86, 66, 78, 77];
 
-const othersWithOutCaps = [192, 189, 187, 8, 9, 219, 221, 220, 20, 186, 222, 13, 16, 188, 190, 191, 16, 17, 91, 18, 32, 18, 93, 17];
+const othersWithOutCaps = [192, 189, 187, 8, 9, 219, 221, 220, 20, 186, 222, 13, 16, 188, 190, 191, 
+                           16, 17, 91, 18, 32, 18, 93, 17];
 
 
 //arr with nums
@@ -40,12 +42,15 @@ for (let i = 0; i < otWhOuCa.length; i++) {
 let j = 0;
 
 document.querySelector('input').addEventListener('keydown', (event) => {
+    
     if (numAndLetter.includes(event.keyCode)) {
         document.querySelectorAll('.nums-and-letters').forEach((element) => {
             element.classList.remove('pressed');
         });
         document.querySelector(`.nums-and-letters[data='${event.keyCode}']`).classList.add('pressed');
+        
     }
+    
     else if (othersWithOutCaps.includes(event.keyCode)) {
         if (event.keyCode == 20) {
             j++;
@@ -68,6 +73,7 @@ document.querySelector('input').addEventListener('keydown', (event) => {
 
 //remove class pressed
 document.querySelector('input').addEventListener('keyup', (event) => {
+    
     document.querySelectorAll('.nums-and-letters').forEach((element) => {
         element.classList.remove('pressed');
     });
@@ -77,6 +83,7 @@ document.querySelector('.capslk').classList.remove('without-caps');
 
 
 document.querySelector('input').addEventListener('keyup', (event) => {
+    
     document.querySelectorAll('.without-caps').forEach((element) => {
         element.classList.remove('pressed');
     });

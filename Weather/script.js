79f542840e1bc918b6a5a085e38e60630ce51cd7
Liubomyr <43443223+Lub4ikChe@users.noreply.github.com>
2020-01-23 12:42:09 +0200

    Update script.js

--- Weather/script.js
+++ Weather/script.js
@@ -1,3 +1,9 @@
+/**
+ * Weather app v 1.0
+ * made by Lub4ikChe
+ */
+
+//all needed vars
 const btnGo = document.querySelector('.go');
 let inputtedCityName = '';
 let cityName = document.querySelector('.city-name');
@@ -8,32 +14,38 @@ let visibility = document.querySelector('.visibility');
 let divImage = document.querySelector('.image');
 
 btnGo.onclick = () => {
+    // city name
     inputtedCityName = document.querySelector('input').value;
-    console.log(inputtedCityName);
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputtedCityName}&appid=9d7ea6f1e3843ea43e6b631fe2e0ea20`)
+    
+    // response
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputtedCityName}&appid=( secret :) ) `)
         .then(function (resp) {
             return resp.json(); //data
         })
         .then(function (data) {
+        
             if (data.message == 'city not found') {
                 document.querySelector('input').value = '';
                 alert("city not found");
             }
+        
             else {
-                console.log(data);
+                                      //name of the city
                 cityName.innerHTML = `Назва міста:<br>${data.name}`;
+                                   //temperature
                 temp.innerHTML = `Температура:<br>${Math.round(data.main.temp - 273)}&deg;`;
+                                        //fell like
                 tempFeelLike.innerHTML = `Відчувається як:<br>${Math.round(data.main.feels_like - 273)}&deg;`;
+                                        //img that describes current weather
                 divImage.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+                                    //speed of wind
                 wind.innerHTML = `Швидкість вітру:<br>${data.wind['speed']}`;
+                                        //visibility
                 visibility.innerHTML = `Видимість<br>${data.visibility}`;
-                // document.querySelector('input').value = '';
-               
-
             }
         })
         .catch(function (e) {
-         
+         // cathc any errors
             console.log(e);
         });
 
